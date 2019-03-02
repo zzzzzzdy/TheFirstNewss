485225febf8af6f9702d0ebb5595855c783cc724
zzzzzzdy <40272365+zzzzzzdy@users.noreply.github.com>
2019-03-02 16:31:17 +0800

    Create Data.java

---
+++ app/src/main/java/activitytest/exmaple/com/Data.java
@@ -0,0 +1,22 @@
+package activitytest.exmaple.com.thefirstshoes;
+
+public class Data {
+    private String url;
+    private String talks;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTalks() {
+        return talks;
+    }
+
+    public Data(String url, String talks) {
+
+        this.url = url;
+        this.talks = talks;
+    }
+
+
+}
