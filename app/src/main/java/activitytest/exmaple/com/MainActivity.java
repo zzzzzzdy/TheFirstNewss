d96d7e5d366d8eee9fa6c1e54e857887ec402731
zzzzzzdy <40272365+zzzzzzdy@users.noreply.github.com>
2019-03-05 21:09:21 +0800

    添加了搜索功能，并在searchactivity里实现上拉刷新

--- app/src/main/java/activitytest/exmaple/com/MainActivity.java
+++ app/src/main/java/activitytest/exmaple/com/MainActivity.java
@@ -1,5 +1,7 @@
 package activitytest.exmaple.com.thefirstshoes;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Bitmap;
@@ -17,7 +19,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     List<Fruit> datas=new ArrayList<Fruit>();
     private FruitAdapter adapter;
     private SwipeRefreshLayout swipeRefresh;
+    private LinearLayoutManager mLayoutManager;
 
 
 
@@ -125,6 +130,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar,menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.ab_search).getActionView();
+        searchView.setQueryHint("请输入8位日期");
+        searchView.setIconified(true);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Intent intent = new Intent(MainActivity.this,SearchedActivity.class);
+                intent.putExtra("Sdate",s);
+                startActivity(intent);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+
         return true;
     }
 
